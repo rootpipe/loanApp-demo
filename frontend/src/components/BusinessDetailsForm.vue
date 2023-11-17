c41880989e6b04385640f107b594ec6c759dd7a9
rootpipe <mehul.mxm@gmail.com>
2023-11-17 16:23:56 +0530

    ui changes and validations

--- frontend/src/components/BusinessDetailsForm.vue
+++ frontend/src/components/BusinessDetailsForm.vue
@@ -1,56 +1,20 @@
-<template>
-  <form @submit.prevent="submitForm" class="space-y-4">
-    <div>
-      <label for="businessName" class="block text-sm font-medium text-gray-700">
-        Business Name
-      </label>
-      <input
-        id="businessName"
-        v-model="businessName"
-        type="text"
-        class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-        placeholder="Enter business name"
-        required
-      />
-    </div>
-
-    <div>
-      <label
-        for="yearEstablished"
-        class="block text-sm font-medium text-gray-700"
-      >
-        Year Established
-      </label>
-      <input
-        id="yearEstablished"
-        v-model.number="yearEstablished"
-        type="number"
-        class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-        placeholder="Enter establishment year"
-        required
-      />
-    </div>
-
-    <button
-      type="submit"
-      class="px-4 py-2 bg-indigo-600 hover:bg-indigo-700 text-white font-bold rounded-md"
-    >
-      Submit
-    </button>
-  </form>
-</template>
-
 <script setup>
 import { ref } from "vue";
 import { submitLoanApplication } from "../services/apiService";
-
+import AccountingSoftwareSelector from "../components/AccountingSoftwareSelector.vue";
+import BalanceSheetDisplay from "../components/BalanceSheetDisplay.vue";
 const businessName = ref("");
 const yearEstablished = ref("");
+const isLoading = ref(false);
+const errorMessage = ref("");
+const preAssessment = ref(null);
+const currentYear = new Date().getFullYear();
+const minYear = 1900;
 const balanceSheet = ref([
   {
     year: 2020,
     month: 12,
-    profitOrLoss: 250000,
+    profitOrLoss: -250000,
     assetsValue: 1234,
   },
   {
@@ -73,6 +37,10 @@ const balanceSheet = ref([
   },
 ]);
 const submitForm = async () => {
+  isLoading.value = true;
+  errorMessage.value = "";
+  preAssessment.value = null;
+
   const applicationData = {
     businessName: businessName.value,
     yearEstablished: yearEstablished.value,
@@ -81,9 +49,83 @@ const submitForm = async () => {
 
   try {
     const response = await submitLoanApplication(applicationData);
-    console.log("Response from server:", response);
+
+    if (Object.prototype.hasOwnProperty.call(response, "pre_assessment")) {
+      preAssessment.value = response.pre_assessment;
+    } else {
+      preAssessment.value = 0;
+      console.warn("Pre-assessment data missing in the response");
+    }
   } catch (error) {
+    errorMessage.value = "Failed to submit application.";
     console.error("Error submitting application:", error);
+  } finally {
+    isLoading.value = false;
   }
 };
 </script>
+<template>
+  <div class="flex justify-center items-center min-h-screen bg-gray-100">
+    <div
+      class="w-full max-w-lg p-6 bg-white rounded-lg border shadow-md sm:p-8 lg:p-10"
+    >
+      <div v-if="errorMessage" class="text-red-700">
+        <p>
+          <strong>Pre-Assessment Value: </strong>
+          <strong>{{ errorMessage }}</strong>
+        </p>
+      </div>
+      <div v-if="preAssessment" class="text-green-700">
+        <p>
+          <strong>Pre-Assessment Value:</strong>
+          <strong>{{ preAssessment }}</strong>
+        </p>
+      </div>
+      <form @submit.prevent="submitForm" class="space-y-4">
+        <div>
+          <label
+            for="businessName"
+            class="block text-sm font-medium text-gray-700"
+          >
+            Business Name
+          </label>
+          <input
+            id="businessName"
+            v-model="businessName"
+            type="text"
+            class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+            placeholder="Enter business name"
+            required
+          />
+        </div>
+
+        <div>
+          <label
+            for="yearEstablished"
+            class="block text-sm font-medium text-gray-700"
+          >
+            Year Established
+          </label>
+          <input
+            id="yearEstablished"
+            v-model.number="yearEstablished"
+            type="number"
+            class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+            placeholder="Enter establishment year"
+            :min="minYear"
+            :max="currentYear"
+            required
+          />
+        </div>
+        <AccountingSoftwareSelector />
+        <BalanceSheetDisplay />
+        <button
+          type="submit"
+          class="w-full px-5 py-3 text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 rounded-lg"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  </div>
+</template>
